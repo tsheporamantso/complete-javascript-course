2979bef8df2c07cbd407899ad3eccdb80418f12a
tsheporamantso <tshepo.ramantso@outlook.com>
2024-07-18 14:30:14 +0200

    keypress event

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,7 +4,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+// console.log(btnsOpenModal);
 
 const openModal = () => {
   modal.classList.remove('hidden');
@@ -22,3 +22,10 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', e => {
+  // console.log(e);
+  e.key === 'Escape' && !modal.classList.contains('hidden')
+    ? closeModal()
+    : null;
+});
