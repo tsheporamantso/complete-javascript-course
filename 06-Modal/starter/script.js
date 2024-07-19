2c47641a0d0312088b963c4dabba41c93ac326e7
tsheporamantso <tshepo.ramantso@outlook.com>
2024-07-19 14:37:45 +0200

    Add comments/notes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,24 +4,30 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 const btnCloseModal = document.querySelector('.close-modal');
-// console.log(btnsOpenModal);
 
-const openModal = () => {
-  modal.classList.remove('hidden');
+/*
+    ? You can choose to either use CSS style or classList to either show or hide the model.
+    ? I have used the both alternatives to illustration purposes.
+    ? classList is much more concise.   
+*/
+const handleOpenModal = () => {
+  // modal.classList.remove('hidden');
+  modal.style.display = 'block';
   overlay.classList.remove('hidden');
 };
 
-const closeModal = () => {
-  modal.classList.add('hidden');
+const handleCloseModal = () => {
+  // modal.classList.add('hidden');
+  modal.style.display = 'none';
   overlay.classList.add('hidden');
 };
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', openModal);
+  btnsOpenModal[i].addEventListener('click', handleOpenModal);
 }
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+btnCloseModal.addEventListener('click', handleCloseModal);
+overlay.addEventListener('click', handleCloseModal);
 
 /*
   ? Keyboard events are called "Global events" and to listen to such events you call the entire document.
@@ -30,6 +36,6 @@ overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', (e) => {
   // console.log(e);
   e.key === 'Escape' && !modal.classList.contains('hidden')
-    ? closeModal()
+    ? handleCloseModal()
     : null;
 });
