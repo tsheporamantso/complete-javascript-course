9f8a584a412ba8e453f20b955f7630de2e2c25a2
tsheporamantso <tshepo.ramantso@outlook.com>
2024-07-19 15:19:30 +0200

    Rename event handlers

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -2,32 +2,32 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
 const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 /*
     ? You can choose to either use CSS style or classList to either show or hide the model.
     ? I have used the both alternatives to illustration purposes.
     ? classList is much more concise.   
 */
-const handleOpenModal = () => {
-  // modal.classList.remove('hidden');
-  modal.style.display = 'block';
+const handleOpenModalClick = () => {
+  // modal.style.display = 'block';
+  modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-const handleCloseModal = () => {
-  // modal.classList.add('hidden');
-  modal.style.display = 'none';
+const handleCloseModalClick = () => {
+  // modal.style.display = 'none';
+  modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', handleOpenModal);
+  btnsOpenModal[i].addEventListener('click', handleOpenModalClick);
 }
 
-btnCloseModal.addEventListener('click', handleCloseModal);
-overlay.addEventListener('click', handleCloseModal);
+btnCloseModal.addEventListener('click', handleCloseModalClick);
+overlay.addEventListener('click', handleCloseModalClick);
 
 /*
   ? Keyboard events are called "Global events" and to listen to such events you call the entire document.
@@ -36,6 +36,6 @@ overlay.addEventListener('click', handleCloseModal);
 document.addEventListener('keydown', (e) => {
   // console.log(e);
   e.key === 'Escape' && !modal.classList.contains('hidden')
-    ? handleCloseModal()
+    ? handleCloseModalClick()
     : null;
 });
