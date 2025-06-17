0d99c39fdd5c6996e72b3053cf26222b514e5eb9
tsheporamantso <develop@molotovcocktail.tv>
2025-06-17 20:25:07 +0200

    Add element selection logic for score and dice display

--- 07-Pig-Game/pigGame/script.js
+++ 07-Pig-Game/pigGame/script.js
@@ -1,1 +1,11 @@
 "use strick";
+import getElement from "./utils/getElement.js";
+
+// *** Selecting Elements ***
+const score0El = getElement("#score--0");
+const score1El = getElement("#score--1");
+const diceEl = getElement(".dice");
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
