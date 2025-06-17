66d3ce9e6be760408e47f567293df222e0a7c67b
tsheporamantso <develop@molotovcocktail.tv>
2025-06-17 16:36:53 +0200

    Refactor modal functionality to use toggle for improved readability and maintainability

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,40 +1,37 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
+// import getElement from '../../05-Guess-My-Number/game/getElement.js';
 
+const showModalBtns = document.querySelectorAll('.show-modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay ');
 /*
     ? You can choose to either use CSS style or classList to either show or hide the model.
     ? I have used the both alternatives to illustration purposes.
     ? classList is much more concise.   
 */
-const OpenModal = () => {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+
+const toggleModal = (element, classList) => {
+  element.classList.toggle(classList);
 };
 
-const CloseModal = () => {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
+showModalBtns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    toggleModal(modal, 'hidden');
+    toggleModal(overlay, 'hidden');
+  });
+});
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', OpenModal);
-}
-
-btnCloseModal.addEventListener('click', CloseModal);
-overlay.addEventListener('click', CloseModal);
+closeModalBtn.addEventListener('click', () => {
+  toggleModal(modal, 'hidden');
+  toggleModal(overlay, 'hidden');
+});
 
 /*
   ? Keyboard events are called "Global events" and to listen to such events you call the entire document.
-  ? Keyboard you have 1) keyup[happens when you lift finger off keyboar], 
-  ?2) keydown[happens when you press down key] and 
-  ?3) keypress[Goes continuously as we keep finger on keyboard.] 
+  ? Keyboard you have 
+  ? 1) keyup[happens when you lift finger off keyboar], 
+  ? 2) keydown[happens when you press down key] and 
+  ? 3) keypress[Goes continuously as we keep finger on keyboard.] 
 */
-document.addEventListener('keydown', (e) => {
-  e.key === 'Escape' && !modal.classList.contains('hidden')
-    ? CloseModal()
-    : null;
-});
